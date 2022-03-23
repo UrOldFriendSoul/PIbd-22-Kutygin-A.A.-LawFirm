e34b17f1014659be03817a5cf2c8cc67bae04d97
UrOldFriendSoul <kutygin.andrey@gmail.com>
2022-03-23 22:03:06 +0400

    Lab1

---
+++ LawFirm/LawFirmFileImplement/FileDataListSingleton.cs
@@ -0,0 +1,178 @@
+﻿using LawFirmContracts.Enums;
+using LawFirmFileImplement.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace LawFirmFileImplement
+{
+    public class FileDataListSingleton
+    {
+        private static FileDataListSingleton instance;
+        private readonly string ComponentFileName = "Component.xml";
+        private readonly string OrderFileName = "Order.xml";
+        private readonly string DocumentFileName = "Document.xml";
+        public List<Component> Components { get; set; }
+        public List<Order> Orders { get; set; }
+        public List<Document> Documents { get; set; }
+        private FileDataListSingleton()
+        {
+            Components = LoadComponents();
+            Orders = LoadOrders();
+            Documents = LoadDocuments();
+        }
+        public static FileDataListSingleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new FileDataListSingleton();
+            }
+            return instance;
+        }
+        ~FileDataListSingleton()
+        {
+            SaveComponents();
+            SaveOrders();
+            SaveDocuments();
+        }
+        private List<Component> LoadComponents()
+        {
+            var list = new List<Component>();
+            if (File.Exists(ComponentFileName))
+            {
+                var xDocument = XDocument.Load(ComponentFileName);
+                var xElements = xDocument.Root.Elements("Component").ToList();
+
+                foreach (var elem in xElements)
+                {
+                    list.Add(new Component
+                    {
+                        Id = Convert.ToInt32(elem.Attribute("Id").Value),
+                        ComponentName = elem.Element("ComponentName").Value
+                    });
+                }
+            }
+                return list;
+        }
+        private List<Order> LoadOrders()
+        {
+            var list = new List<Order>();
+            if (File.Exists(OrderFileName))
+            {
+                var xDocument = XDocument.Load(OrderFileName);
+                var xElements = xDocument.Root.Elements("Order").ToList();
+                foreach (var elem in xElements)
+                {
+                    bool dateimplement = elem.Element("DateImplement").IsEmpty;
+                    list.Add(new Order
+                    {
+                        Id = Convert.ToInt32(elem.Attribute("Id").Value),
+                        DocumentId = Convert.ToInt32(elem.Element("DocumentId").Value),
+                        Count = Convert.ToInt32(elem.Element("Count").Value),
+                        Sum = Convert.ToDecimal(elem.Element("Sum").Value),
+                        Status = (OrderStatus)Enum.Parse(typeof(OrderStatus), elem.Element("Status").Value),
+                        DateCreate = Convert.ToDateTime(elem.Element("DateCreate").Value),
+                        DateImplement = dateimplement ? null : Convert.ToDateTime(elem.Element("DateImplement").Value)
+                    });
+                }
+            }
+            return list;
+        }
+        private List<Document> LoadDocuments()
+        {
+            var list = new List<Document>();
+            if (File.Exists(DocumentFileName))
+            {
+                var xDocument = XDocument.Load(DocumentFileName);
+                var xElements = xDocument.Root.Elements("Document").ToList();
+                foreach (var elem in xElements)
+                {
+                    var docComp = new Dictionary<int, int>();
+                    foreach (var component in
+                   elem.Element("DocumentComponents").Elements("DocumentComponent").ToList())
+                    {
+                        docComp.Add(Convert.ToInt32(component.Element("Key").Value),
+                       Convert.ToInt32(component.Element("Value").Value));
+                    }
+                    list.Add(new Document
+                    {
+                        Id = Convert.ToInt32(elem.Attribute("Id").Value),
+                        DocumentName = elem.Element("DocumentName").Value,
+                        Price = Convert.ToDecimal(elem.Element("Price").Value),
+                        DocumentComponents = docComp
+                    });
+                }
+            }
+            return list;
+        }
+        private void SaveComponents()
+        {
+            if (Components != null)
+            {
+                var xElement = new XElement("Components");
+                foreach (var component in Components)
+                {
+                    xElement.Add(new XElement("Component",
+                    new XAttribute("Id", component.Id),
+                    new XElement("ComponentName", component.ComponentName)));
+                }
+                var xDocument = new XDocument(xElement);
+                xDocument.Save(ComponentFileName);
+            }
+        }
+        private void SaveOrders()
+        {
+            if (Orders != null)
+            {
+                var xElement = new XElement("Orders");
+                foreach (var order in Orders)
+                {
+                    xElement.Add(new XElement("Order",
+                    new XAttribute("Id", order.Id),
+                    new XElement("DocumentId", order.DocumentId),
+                    new XElement("Count", order.Count),
+                    new XElement("Sum", order.Sum),
+                    new XElement("Status", order.Status),
+                    new XElement("DateCreate", order.DateCreate),
+                    new XElement("DateImplement", order.DateImplement)));
+                }
+                var xDocument = new XDocument(xElement);
+                xDocument.Save(OrderFileName);
+            }
+        }
+        private void SaveDocuments()
+        {
+            if (Documents != null)
+            {
+                var xElement = new XElement("Documents");
+                foreach (var document in Documents)
+                {
+                    var compElement = new XElement("DocumentComponents");
+                    foreach (var component in document.DocumentComponents)
+                    {
+                        compElement.Add(new XElement("DocumentComponent",
+                        new XElement("Key", component.Key),
+                        new XElement("Value", component.Value)));
+                    }
+                    xElement.Add(new XElement("Document",
+                     new XAttribute("Id", document.Id),
+                     new XElement("DocumentName", document.DocumentName),
+                     new XElement("Price", document.Price),
+                     compElement));
+                }
+                var xDocument = new XDocument(xElement);
+                xDocument.Save(DocumentFileName);
+            }
+        }
+        public static void Save()
+        {
+            instance.SaveOrders();
+            instance.SaveDocuments();
+            instance.SaveComponents();
+        }
+    }
+}
+
+
---
+++ LawFirm/LawFirmView/FormCreateOrder.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace LawFirmView
+{
+    partial class FormCreateOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.comboBoxDocument = new System.Windows.Forms.ComboBox();
+            this.textBoxCount = new System.Windows.Forms.TextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.labelSum = new System.Windows.Forms.Label();
+            this.textBoxSum = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(13, 13);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(64, 15);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Документ:";
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Location = new System.Drawing.Point(12, 49);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(75, 15);
+            this.labelCount.TabIndex = 1;
+            this.labelCount.Text = "Количество:";
+            // 
+            // comboBoxDocument
+            // 
+            this.comboBoxDocument.FormattingEnabled = true;
+            this.comboBoxDocument.Location = new System.Drawing.Point(94, 10);
+            this.comboBoxDocument.Name = "comboBoxDocument";
+            this.comboBoxDocument.Size = new System.Drawing.Size(273, 23);
+            this.comboBoxDocument.TabIndex = 3;
+            this.comboBoxDocument.TextChanged += new System.EventHandler(this.ComboBoxDocument_SelectedIndexChanged);
+
+            // 
+            // textBoxCount
+            // 
+            this.textBoxCount.Location = new System.Drawing.Point(93, 46);
+            this.textBoxCount.Name = "textBoxCount";
+            this.textBoxCount.Size = new System.Drawing.Size(273, 23);
+            this.textBoxCount.TabIndex = 4;
+            this.textBoxCount.TextChanged += new System.EventHandler(this.TextBoxCount_TextChanged);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(181, 122);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 6;
+            this.buttonSave.Text = "Сохранить";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(263, 122);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 7;
+            this.buttonCancel.Text = "Отмена";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // labelSum
+            // 
+            this.labelSum.AutoSize = true;
+            this.labelSum.Location = new System.Drawing.Point(13, 81);
+            this.labelSum.Name = "labelSum";
+            this.labelSum.Size = new System.Drawing.Size(48, 15);
+            this.labelSum.TabIndex = 2;
+            this.labelSum.Text = "Сумма:";
+            // 
+            // textBoxSum
+            // 
+            this.textBoxSum.Enabled = false;
+            this.textBoxSum.Location = new System.Drawing.Point(94, 78);
+            this.textBoxSum.Name = "textBoxSum";
+            this.textBoxSum.Size = new System.Drawing.Size(272, 23);
+            this.textBoxSum.TabIndex = 5;
+            this.textBoxSum.TextChanged += new System.EventHandler(this.FormCreateOrder_Load);
+            // 
+            // FormCreateOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 166);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textBoxSum);
+            this.Controls.Add(this.textBoxCount);
+            this.Controls.Add(this.comboBoxDocument);
+            this.Controls.Add(this.labelSum);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.labelName);
+            this.Name = "FormCreateOrder";
+            this.Text = "FormCreateOrder";
+            this.Load += new System.EventHandler(this.FormCreateOrder_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label labelName;
+        private Label labelCount;
+        private ComboBox comboBoxDocument;
+        private TextBox textBoxCount;
+        private Button buttonSave;
+        private Button buttonCancel;
+        private Label labelSum;
+        private TextBox textBoxSum;
+    }
+}
