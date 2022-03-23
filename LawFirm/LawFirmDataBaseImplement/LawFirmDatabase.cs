7cf9232e53779f9cc1f873b682f2e56c26f35d07
UrOldFriendSoul <kutygin.andrey@gmail.com>
2022-03-23 23:36:55 +0400

    Lab3

---
+++ LawFirm/LawFirmDataBaseImplement/LawFirmDatabase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LawFirmDatabaseImplement.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace LawFirmDatabaseImplement
+{
+    public class LawFirmDatabase : DbContext
+    {
+        public static void Main()
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+         {
+             if (optionsBuilder.IsConfigured == false)
+             {
+             optionsBuilder.UseSqlServer(@"Data Source=LAPTOP-B2TPTK14\SQLEXPRESS01;Initial Catalog=LawFirmDatabase;Integrated Security=True;MultipleActiveResultSets=True;");
+             }
+             base.OnConfiguring(optionsBuilder);
+         }
+         public virtual DbSet<Component> Components { set; get; }
+         public virtual DbSet<Document> Documents { set; get; }
+         public virtual DbSet<DocumentComponent> DocumentComponents { set; get; }
+         public virtual DbSet<Order> Orders { set; get; }
+
+    }
+}
---
+++ LawFirm/LawFirmDataBaseImplement/Models/Component.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace LawFirmDatabaseImplement.Models
+{
+    public class Component
+    {
+        public int Id { get; set; }
+        [Required]
+        public string ComponentName { get; set; }
+        [ForeignKey("ComponentId")]
+        public virtual List<DocumentComponent> DocumentComponents { get; set; }
+    }
+}
