a46403611fd295f502bdfea5512746605bec6854
UrOldFriendSoul <kutygin.andrey@gmail.com>
2022-03-23 23:26:09 +0400

    Lab1

---
+++ LawFirm/LawFirmContracts/StorageContracts/IDocumentStorage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using LawFirmContracts.BindingModels;
+using LawFirmContracts.ViewModels;
+
+namespace LawFirmContracts.StorageContracts
+{
+    public interface IDocumentStorage
+    {
+        List<DocumentViewModel> GetFullList();
+        List<DocumentViewModel> GetFilteredList(DocumentBindingModel model);
+        DocumentViewModel GetElement(DocumentBindingModel model);
+        void Insert(DocumentBindingModel model);
+        void Update(DocumentBindingModel model);
+        void Delete(DocumentBindingModel model);
+    }
+}
